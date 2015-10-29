37a8b3b44e0b07dbd53f249b83c01f0aeddbe325
Subahu Rayamajhi <subahur@brandeis.edu>
2015-10-28 22:45:52 -0400

    nav bar working

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+
+helpers do
+  
+  def render(*args)
+    if args.first.is_a?(Hash) && args.first.keys.include?(:partial)
+      return haml "_#{args.first[:partial]}".to_sym, :layout => false
+    else
+      super
+    end
+  
+  end
+end
+
+
 get '/' do
 	erb :home
 end
