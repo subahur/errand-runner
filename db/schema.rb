a541427b1741092c7a337be44b7821f72c32d15f
Subahu Rayamajhi <subahur@brandeis.edu>
2015-11-03 01:05:57 -0500

    ;

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20151102050432) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ 
 
   create_table "users", force: :cascade do |t|
     t.string "name"
